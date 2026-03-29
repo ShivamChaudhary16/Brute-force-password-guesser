86d03e9db171c17118cb3ddf4ef388960285be41
SHIVAM <135863658+ShivamChaudhary16@users.noreply.github.com>
2026-03-29 19:00:59 +0530

    Add files via upload

---
+++ stringpass_guesser.py
@@ -0,0 +1,25 @@
+import itertools
+import string
+import time
+characters=string.ascii_letters+ string.digits+ string.punctuation +" "
+# Input taken from user
+password=input("Enter the password: ")
+n=len(password)
+# Brute force password guesser
+start_time=time.time()
+guesses=itertools.product(characters,repeat=n)
+attempt_counter=0
+for guess in guesses:
+    guessed_pass="".join(guess)
+    attempt_counter+=1
+    if password==guessed_pass:
+        end_time=time.time()
+        print(f"Password is {password}")
+        print(f"Attempt taken: {attempt_counter}")
+        print(f"Time taken:{end_time-start_time} seconds")
+        break
+else:
+    print("Password not found")    
+
+ #how does the program know the length of the password? if its unknown, i.e i need to make a program in which it doesnt depend on the length of the password
+     
